40e4a164a44a99229c8b67622e6d804374d5c1c9
T-wrecks <tameredits@gmail.com>
2011-10-05 18:13:22 -0400

    Fitst Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e6a521111b6fe850a75db615dc3439b6f873ee240d0f259c010d52fd4b683afe3cf816ae0173f37421abbedbec2ee6da11f4a1ac23a40e12de0081039e7bb76d'
