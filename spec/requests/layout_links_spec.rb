a7234605392130770803bccfc31c866f518c1e83
T-wrecks <tameredits@gmail.com>
2011-10-11 16:04:57 -0400

    Added Integration Testing and Users Controller

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,5 +22,24 @@ describe "LayoutLinks" do
     response.should have_selector("title", :content => 'Help')
   end
   
+  it "should have a Sign up page at '/signup'" do
+    get "/signup"
+    response.should have_selector("title", :content => 'Sign Up')
+  end
+  
+  it "should have the correct links on layout" do
+    visit root_path
+    response.should have_selector("title", :content => 'Home')
+    click_link "About"
+    response.should have_selector("title", :content => 'About')
+    click_link "Contact"
+    response.should have_selector("title", :content => 'Contact')
+    click_link "Home"
+    response.should have_selector("title", :content => 'Home')
+    click_link "Sign up now!"
+    response.should have_selector("title", :content => 'Sign Up')
+    response.should have_selector('a[href="/"]>img')
+  end
+  
 end
   
